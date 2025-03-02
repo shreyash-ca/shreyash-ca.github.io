7d7a653f9f4b648d52c4821b7f466fc362de6d34
Ashutosh Tiwari <findashutoshtiwari@gmail.com>
2025-03-02 08:08:12 -0800

    temp

--- js/script.js
+++ js/script.js
@@ -25,33 +25,68 @@ $(document).on('click', function(){
     console.log('Shaadi me zaroor aana');
 });
 
-// Set the date we're counting down to
-var countDownDate = new Date("Mar 11, 2025 20:00:00").getTime();
+// Countdown Timer
+document.addEventListener('DOMContentLoaded', function() {
+    try {
+        // Set the date we're counting down to
+        var countDownDate = new Date("Mar 11, 2025 20:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="time"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time").innerHTML = "Celebration in progress! Bless the couple for a happy life!";
+        console.log("Countdown initialized for:", new Date(countDownDate));
+        
+        // Get the timer element
+        var timerElement = document.getElementById("time");
+        
+        if (!timerElement) {
+            console.error("Timer element not found!");
+            return;
+        }
+        
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+            // Get today's date and time
+            var now = new Date().getTime();
+            
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
+            
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+            // Output the result in an element with id="time"
+            timerElement.innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+            
+            console.log("Countdown updated:", days, "days", hours, "hours", minutes, "minutes", seconds, "seconds");
+            
+            // If the count down is over, write some text 
+            if (distance < 0) {
+                clearInterval(x);
+                timerElement.innerHTML = "Celebration in progress! Bless the couple for a happy life!";
+            }
+        }, 1000);
+        
+        // Run it once immediately to avoid delay
+        if (timerElement) {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+            timerElement.innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+        }
+    } catch (error) {
+        console.error("Error in countdown script:", error);
+        // Display a fallback message if there's an error
+        var timerElement = document.getElementById("time");
+        if (timerElement) {
+            timerElement.innerHTML = "Counting down to our wedding on March 11, 2025!";
+        }
     }
-}, 1000);
+});
 
 // being a bit cool :p  
 var styles = [
